87d5aa843157a527561feb4c6428bb5ddd33e4e0
Ryan de Lima <93151061+Ryan-de-Lima-Pereira@users.noreply.github.com>
2022-05-05 16:07:21 -0300

    Add files via upload
    Melhorias no formulário num geral, mas com um erro no envio da requisição

--- javascript.js
+++ javascript.js
@@ -3,9 +3,20 @@ class Formulario {
         this.formulario = document.querySelector('.formulario');
         this.titulo = document.querySelector('.titulo');
         this.textoLink = document.querySelector('.textoLink');
-        this.eventos();
+        this.botaoDiv = document.querySelector('.botaoDiv');
+        this.eventosLogin();
     }
+    // Coisas a se fazer
+    /*  
+    Verificador de campos
+    Requisição POST (registro) ????? Tem erro 415
+    Requisição GET (login)
+    
+    */
 
+    conectar() {
+        console.log('Conectando')
+    }
 
     registro() {
         // Pegando os dados
@@ -14,36 +25,48 @@ class Formulario {
         const senha2 = this.formulario.querySelector('#confirmarSenha').value;
         const generoM = this.formulario.querySelector('#masculino');
         let genero
-
-        // Pegando o genero
-        if(generoM.checked) {
-            genero = 'masculino';
-        }else {
-            genero = 'feminino';
-        };
-
-        // Convertendo para JSON
-        const obj = {username: user, password: senha, gender: genero};
-
-        const JsonStr = JSON.stringify(obj);
-
-        // Configurando o parametro para o Fetch
-        const confReg = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            Json: JsonStr,
-        };
-
-        // Requisição post
         
-    }
+        // Verificar os campos
+        if (this.campoOk()) {
+            // Pegando o genero
+            if(generoM.checked) {
+                genero = 'masculino';
+            }else {
+                genero = 'feminino';
+            };
+    
+            // Convertendo para JSON
+            const obj = {username: user, password: senha, gender: genero};
+            console.log(JSON.stringify(obj));
+    
+            // Configurando o parametro para o Fetch
+    
+            const confReg = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(obj),
+                mode: 'no-cors'
+            };
+    
+            // Requisição post
+            fetch('https://crudapiproject.azurewebsites.net/api/user/register', confReg)
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err))
+
+        }else{
+            console.log('Deu errado bicho');
+        };
+        
+    };
 
 
     troca() {
         // Dados
         const registroField = this.formulario.querySelectorAll('.registro');
+        
         
         // Verificar qual o estado está
         if (this.titulo.innerHTML == 'Login'){
@@ -60,7 +83,18 @@ class Formulario {
             // Trocando a frase do Link
             this.textoLink.innerHTML = 'Já possui conta? Entre com ela <a class="link">clicando aqui</a>.' 
 
-            this.eventos();
+            // Fazendo o botão registrar
+
+            this.botaoDiv.innerHTML = '<div class="botao"><p>Registrar</p></div>'
+
+            this.formulario.querySelector('.botao').addEventListener('click', e => {
+                this.registro();
+            });
+
+            // Colocando listener no link novamente
+            this.textoLink.querySelector('.link').addEventListener('click', e => {
+                this.troca();
+            })
 
 
         }else{
@@ -77,19 +111,61 @@ class Formulario {
             // Trocando a frase do Link
             this.textoLink.innerHTML = 'Não possui uma conta? Faça uma <a class="link">clicando aqui</a>.' 
 
-            this.eventos();
+            // Preparando o botão
+            this.botaoDiv.innerHTML = '<div class="botao"><p>Entrar</p></div>'
+
+            this.eventosLogin();
         }
 
     };
 
+    campoOk(){
+        let verificar = true;
+        // Retirando os erros antigos
+        for (let erro of this.formulario.querySelectorAll('.error-text')) {
+            erro.remove();
+        };
 
-    eventos() {
-        this.formulario.querySelector('.botao').addEventListener('click', e => {
-            this.registro();
-        });
-        this.textoLink.querySelector('.link').addEventListener('click', e => {
+        // Campos em branco
+        for (let i of this.formulario.querySelectorAll('.validar')){
+            if (i.value == ''){
+                this.erroForm(i, 'Campo em branco')
+                verificar = false;
+            }
+        };
+
+        // Verificar as senhas
+        if (this.formulario.querySelector('#senha').value != this.formulario.querySelector('#confirmarSenha').value){
+            this.erroForm(this.formulario.querySelector('#senha'), 'Os campos senha e confirmar senha devem ser iguais');
+            this.erroForm(this.formulario.querySelector('#confirmarSenha'), 'Os campos senha e confirmar senha devem ser iguais')
+            verificar = false;
+        }
+
+
+        //console.log(verificar);
+        return verificar;
+    };
+
+    erroForm(campo, frase){
+
+        // Colocando a frase de erro
+        const p = document.createElement('p');
+        p.innerText = frase;
+        p.classList.add('error-text')
+        campo.insertAdjacentElement('afterend', p);
+    };
+    eventosLogin() {
+        const eConectar = e => {
+            this.conectar();
+        };
+
+        const eTroca = e => {
             this.troca();
-        })
+        };
+
+        this.formulario.querySelector('.botao').addEventListener('click', eConectar);
+
+        this.textoLink.querySelector('.link').addEventListener('click', eTroca);
     };
 
 }
